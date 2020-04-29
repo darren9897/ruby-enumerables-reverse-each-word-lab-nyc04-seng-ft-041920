79b606490f18676979073016cb9529cd90e162b7
Darren Koomson <darrenkoomson17@gmail.com>
2020-04-29 19:26:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,11 +3,9 @@ def(sentence)
 sent = sentence.split(" ")
 new_arr = []
 
-sent.each do |sen|
-  new_arr << sen.reverse
-  
-  
-end
+  sent.each do |sen|
+    new_arr << sen.reverse
+  end
   
 
 return new_arr(" ")
